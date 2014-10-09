0211ec39eaaf893360f4ea23126831ca396931ae
Rachel Johnson <2rachy@gmail.com>
2014-10-09 09:52:33 -0600

    Rachel adding date changing functionality.

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -5,7 +5,17 @@ angular.module('starter.controllers', [])
             $scope.date = new Date();
 
         $scope.next = function (){
-
+            $scope.date.setDate($scope.date.getDate()+1);
+            while($scope.date.getDay() == 6 || $scope.date.getDay() == 0)
+            {
+                $scope.date.setDate($scope.date.getDate()+1);
+            }
         }
-
+        $scope.previous = function (){
+            $scope.date.setDate($scope.date.getDate()-1);
+            while($scope.date.getDay() == 6 || $scope.date.getDay() == 0)
+            {
+                $scope.date.setDate($scope.date.getDate()-1);
+            }
+        }
     });
